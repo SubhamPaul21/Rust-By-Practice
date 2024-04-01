7a26b62d4521f3324ad94da5c8c519c63e99bc14
Subham21 <subhampaul12@gmail.com>
2024-04-01 18:40:16 +0530

    modified scope based code

--- src/Variables/Scope_1.rs
+++ src/Variables/Scope_1.rs
@@ -5,8 +5,7 @@ fn main() {
     let x: i32 = 10;
     let y: i32 = 10;
     {
-        let x = 5;
-        let y = 5;
+        let y: i32 = 5;
         println!("The value of x is {} and value of y is {}", x, y);
     }
     println!("The value of x is {} and value of y is {}", x, y);
--- src/Variables/Scope_2.rs
+++ src/Variables/Scope_2.rs
@@ -1,10 +1,19 @@
 // Fix the error with the use of define_x
 fn main() {
-    let x = define_x();
+    let x = define_x1();
     println!("{}, world", x);
 }
 
-fn define_x() -> &'static str {
+// Borrowed Value
+fn define_x1() -> &'static str {
     let x = "hello";
     return x;
+}
+
+// OR
+
+// Owner Value
+fn define_x2() -> String {
+    let x = "hello".to_string();
+    reutrn x;
 }
